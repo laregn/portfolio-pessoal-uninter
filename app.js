e98dd8545ae853cb9d8020f66a68a4945593bd4a
Lays Souza <lays.lays99@outlook.com>
2024-08-16 18:18:04 -0300

    final commit

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
 function retrocederPagina() {
     window.history.back();
 }
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    let name = document.getElementById('name').value;
+    let email = document.getElementById('email').value;
+    let message = document.getElementById('message').value;
+    
+    alert(`Obrigado pelo contato, ${name}! Sua mensagem foi recebida e logo será respondida.`);
+});
